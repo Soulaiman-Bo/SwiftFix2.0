82e00e93db14c6cf264b5926e293f1d7b810e00e
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2023-10-25 17:24:02 +0100

    I added the click to slide feature to the automatic carousel. now it slides automatically every 3 seconds and it slides with a click

--- index.js
+++ index.js
@@ -2,27 +2,45 @@ let cardWraper = document.querySelector('.section__testimonial-cards');
 let cards = document.querySelectorAll('.testimonial__card');
 let dot = document.querySelectorAll('.dot');
 
-
+cardWraper.addEventListener
 
 let scrolling = 0
 let scrollCount = 0
 
-let scrollInterval = setInterval(scroll, 3000)
+let scrollInterval = setInterval(scrollingInterval, 3000)
 
-function scroll() {
+for (let i = 0; i < dot.length; i++) {
+    dot[i].addEventListener("click", () => {
+        // for (let j = 0; j < dot.length; j++) {
+        //     dot[j].classList.remove("active")
+        // }
+        dot[scrollCount].classList.remove("active")
+        scrollCount = i
+        scrollingFn()
+    })
+}
 
+
+function scrollingInterval() {
     dot[scrollCount].classList.remove("active")
-
-    scrollCount += 1;
-
+    scrollCount += 1
     if (scrollCount == 4) {
         clearInterval(scrollInterval);
         scrollCount = 0
-        scrollInterval = setInterval(scroll, 3000)
+        scrollInterval = setInterval(scrollingInterval, 3000)
     }
-    scrolling = scrollCount * 636;
-    dot[scrollCount].classList.add("active")
 
-    cardWraper.style.transform = `translateX(${-scrolling}px)`
-
+    scrollingFn()
 }
+
+
+function scrollingFn() {
+    dot[scrollCount].classList.add("active")
+    let scroll_points = [0, 638, 1272, 1908]
+    cardWraper.style.transform = `translateX(${-scroll_points[scrollCount]}px)`;
+}
+
+
+
+
+
